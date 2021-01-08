34106c9e5ab39fb08009d7d75538321bbab4f1f8
Ken Li <kli22@stuy.edu>
2021-01-08 01:03:42 -0500

    Fixed my add method with an index

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -4,7 +4,7 @@ public class MyLinkedList{
 
   public MyLinkedList(){
     size = 0;
-    start = new Node(null);
+    start = null;
   }
 
   public int size(){
@@ -38,13 +38,23 @@ public class MyLinkedList{
     if ( index < 0 || index > size){
       throw new IndexOutOfBoundsException ("this is not a valid index!");
     }
-    else{
-      while (i < index){
+    else if (index == 0){
+      NewNode.setNext(start);
+      start = NewNode;
+    }
+    else if ( index < size){
+      while (i < index - 1){
         Current = Current.getNext();
+        i = i + 1;
+
       }
       Node PlaceHolder = Current.getNext();
       Current.setNext(NewNode);
       NewNode.setNext(PlaceHolder);
+   	  size = size + 1;
+    }
+    else{
+      add(value);
     }
     return true;
   }
@@ -72,11 +82,12 @@ public class MyLinkedList{
 
   public static void main (String [] args){
     MyLinkedList Test = new MyLinkedList();
-    Test.add("hi");
-    Test.add("hi2");
-    Test.add("hi3");
-    Test.add("hi4");
-    System.out.println(Test.size());
+    Test.add("0");
+    Test.add("1");
+    Test.add("2");
+    Test.add("3");
+    System.out.println(Test.toString());
+    Test.add(2,"4");
     System.out.println(Test.toString());
   }
 }
